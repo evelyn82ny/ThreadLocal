ac5d208edf83c3c54b7a420141804c5ea1347ffc
nayoung <evelyn82ny@gmail.com>
2022-02-16 01:03:51 +0900

    Initial commit

---
+++ src/main/java/nayoung/threadlocal/ThreadLocalApplication.java
@@ -0,0 +1,13 @@
+package nayoung.threadlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadLocalApplication.class, args);
+    }
+
+}
